713339e095f76645d5f0d3ca4bacb5408a03a1b9
R3M1d4Cod3r <superchristian9@gmail.com>
2025-06-07 13:01:21 +0200

    feat: mu event

---
+++ src/main/kotlin/measuremanager/settingsmanager/dtos/EventMU.kt
@@ -0,0 +1,3 @@
+package measuremanager.settingsmanager.dtos
+
+data class EventMU(val eventType: String, val mu: MuCreateDTO)
--- src/main/kotlin/measuremanager/settingsmanager/kafka/KafkaMuConsumer.kt
+++ src/main/kotlin/measuremanager/settingsmanager/kafka/KafkaMuConsumer.kt
@@ -1,6 +1,7 @@
 package measuremanager.settingsmanager.kafka
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import measuremanager.settingsmanager.dtos.EventMU
 import measuremanager.settingsmanager.dtos.MuCreateDTO
 import measuremanager.settingsmanager.services.MuSettingService
 import org.springframework.kafka.annotation.KafkaListener
@@ -9,14 +10,22 @@ import org.springframework.stereotype.Service
 @Service
 class KafkaMuConsumer(private val ms: MuSettingService, private val objectMapper: ObjectMapper) {
 
-    @KafkaListener(topics = ["mu-creation"], groupId = "measurestream")
+    @KafkaListener(topics = ["mus"], groupId = "measurestream")
     fun consume(message: String) {
 
         try {
-            val mucreate = objectMapper.readValue(message, MuCreateDTO::class.java)
-            val data = ms.create(mucreate)
+            val event = objectMapper.readValue(message, EventMU::class.java)
+            when (event.eventType){
+                "CREATE" -> {
+                    val data = ms.create(event.mu)
+                    println("Saved data: $data")
+                }
+                "DELETE" -> {
+                    ms.delete(event.mu.networkId)
+                }
+                else -> {throw Exception("Unrecognized event : $event")}
+            }
 
-            println("Saved data: $data")
         } catch (e: Exception) {
             println("Error parsing message: $message")
             e.printStackTrace()
